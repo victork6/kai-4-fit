5abab2413a53168e4ff982347741d3038f7fbc2d
vDiezma <vicdiesan6@gmail.com>
2023-08-19 14:39:35 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Kai-4-Fit',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
